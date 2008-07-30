925865895e86bb27b44822c061f57944a1cae119
johanliesen <johanliesen@574a02d7-4353-0410-b3e8-a313e37bb655>
2008-07-30 21:06:18 +0000

    Added some code for searching the Google gadgets directory.

---
+++ trunk/js/directory-search.js
@@ -0,0 +1,56 @@
+
+/**
+ * File contains code for searching the Google gadgets directory.
+ */
+
+var RSS_BASE_URL = 'http://www.google.com/ig/directory?sort=popular&output=rss';
+
+var FEATURED_GADGETS_URL = RSS_BASE_URL + '&cat=featured&hl=en&gl=us';
+
+/**
+ * Returns an array of objects representing gadgets. Keys in the objects are:
+ *   link, guid, title, description, thumbnail
+ * ... and corresponds to the nodes with the same names in the RSS feed.
+ */
+function getRadioGadgets() {
+  var rssUrl = buildSearchUrl("radio")
+
+  _IG_FetchXmlContent(rssUrl, function(response) {
+    if (response == null || typeof(response) != 'object' || 
+        response.firstChild == null) {
+      return [];
+    }
+      
+    var gadgets = [];
+    var items = response.getElementsByTagName('item');
+
+    for (var i = 0; i < items.length; ++i) {
+      var item = {
+        'link': getNodeValue(item, 'link'),
+        'guid': getNodeValue(item, 'guid'),
+        'title': getNodeValue(item, 'title),
+        'description': getNodeValue(item, 'description')
+      }
+
+      items.push(item);
+    }
+
+    return items;
+  }
+}
+
+function buildSearchUrl(searchQuery) {
+  return RSS_BASE_URL + '&q=' + searchQuery;
+}
+
+/**
+ * Extracts the value from an XML node that has only one (text) child.
+ */
+function getNodeValue(item, nodeName, opt_defaultValue) {
+  try {
+    return item.getElementsByTagName(nodeName)[0].childNodes[0].nodeValue;
+  } catch(ex) {
+    return opt_defaultValue || '';
+  }
+}
+
