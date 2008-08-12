2d74212ba4fd937a09c8aa8d290197d199c71161
johanliesen <johanliesen@574a02d7-4353-0410-b3e8-a313e37bb655>
2008-08-12 19:05:06 +0000

    refactored js funcs to my-radio.js, registered onload handler

--- js/my-radio.js
+++ js/my-radio.js
@@ -7,3 +7,29 @@ function createEmbedGadgetUrl(gadgetXmlUrl) {
       ['', 'w=320', 'h=60', 'output=iframe'].join('&amp;');
 }
 
+
+function updateRadioList(radioList) {
+  getRadioGadgets(undefined, function (gadgetItem) {
+    if (!isFeed(gadgetItem)) {
+      radioList.appendChild(createOptionElement(gadgetItem));
+    }
+  }
+}
+
+function createOptionElement(gadgetItem) {
+  var element = document.createElement('option');
+
+  element.setAttribute('value', gadgetItem.guid);
+  element.appendChild(document.createTextNode(gadgetItem.title));
+
+  return element;
+}
+
+function showRadio(stationUrl) {
+  _gel('gadget').setAttribute('src', createEmbedGadgetUrl(stationUrl));
+}
+
+function setStation(select) {
+  showRadio(select[select.selectedIndex].value);
+}
+
